46bce42d1bf308abcabeae3e621980f5b9a01c52
Pablo Méndez <valentino.mendez@correo.ucu.edu.uy>
2022-05-07 22:05:59 -0300

    Changes

---
+++ src/Library/FilePrint.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class FilePrint : IPrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+        }
+    }
+}
---
+++ src/Library/IPrint.cs
@@ -0,0 +1,8 @@
+namespace Full_GRASP_And_SOLID.Library
+{
+    public interface IPrinter
+    {
+        public void PrintRecipe(Recipe recipe);
+
+    }
+}
